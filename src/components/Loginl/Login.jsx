524eb6679b2a401e1cd89f9f19977bade8cc888c
Mohammad Rafe <mrrafi5869@gmail.com>
2023-03-17 20:05:40 +0600

    correction

--- src/components/Loginl/Login.jsx
+++ src/components/Loginl/Login.jsx
@@ -39,12 +39,12 @@ const Login = () => {
                     className="input w-full mb-8 bg-gray-200 border"
                     required
                   />
-                  <input
+                  {/* <input
                     type="text"
                     name=""
                     placeholder="@gmail.com"
                     className="input h-10 w-44 absolute right-1 top-1 flex justify-center items-center"
-                  />
+                  /> */}
                 </div>
                 <div className="form-control relative flex justify-center items-center">
                   <input
